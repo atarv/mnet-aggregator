2725dcfab9cf6717deeaa009da2349120a862135
Aleksi Tarvainen <a.aleksi.tarvainen@gmail.com>
2020-06-26 01:10:22 +0300

    Scrape more things (author, description etc.)

--- app/Main.hs
+++ app/Main.hs
@@ -2,26 +2,49 @@
 
 import           Text.HTML.Scalpel
 import           Control.Applicative
+import qualified Text.Regex.TDFA               as RegexTDFA
+
+re :: String -> RegexTDFA.Regex
+re = RegexTDFA.makeRegex
 
 baseUrl = "https://muusikoiden.net"
 
 data Announcement =
-    Announcement { title :: String, link :: String }
+    Announcement { title :: String, link :: String, description :: String
+                 , author :: String, authorId :: String, price :: String
+                 , thumbnails :: [String], dates :: String }
     deriving (Show, Eq)
+
+announcements :: Scraper String [Announcement]
+announcements = chroots "table" announcement
+
+announcement :: Scraper String Announcement
+announcement = do
+    (t, l) <- chroot ("td" @: [hasClass "tori_title"] // "a") $ do
+        t <- text $ tagSelector "a"
+        l <- attr "href" $ tagSelector "a"
+        return (t, l)
+    desc           <- text $ "font" @: [hasClass "msg"]
+    (auth, authId) <- chroot ("a" @: ["href" @=~ (re "/jasenet.*")]) $ do
+        auth   <- text "a"
+        authId <- attr "href" $ "a"
+        return (auth, authId)
+    p    <- texts "p" -- FIXME: Price doesn't get scraped properly
+    pics <- attrs "src" $ "img" @: [hasClass "border"]
+    dat  <- text $ "small" @: [hasClass "light"]
+    return $ Announcement { title       = t
+                          , link        = baseUrl <> l
+                          , description = desc
+                          , author      = auth
+                          , authorId    = authId
+                          , price       = concat p
+                          , thumbnails  = pics
+                          , dates       = dat
+                          }
 
 main :: IO ()
 main = do
-    content <- readFile "/home/aleksi/setit-utf.html" :: IO String
     scrapeURL
             (baseUrl <> "/tori/?type=sell&province=Keski-Suomi&category=20")
-            comments
+            announcements
         >>= print
-  where
-    comments :: Scraper String [Announcement]
-    comments = chroots ("td" @: [hasClass "tori_title"]) announcement
-
-    announcement :: Scraper String Announcement
-    announcement = do
-        t <- text $ tagSelector "a"
-        l <- attr "href" $ tagSelector "a"
-        return $ Announcement { title = t, link = baseUrl <> l }
