fa6b2d493f227a15d1ecb20d14405ef92e840030
Aleksi Tarvainen <a.aleksi.tarvainen@gmail.com>
2020-06-26 15:08:20 +0300

    Reorder code

--- app/Main.hs
+++ app/Main.hs
@@ -10,41 +10,51 @@ re = RegexTDFA.makeRegex
 baseUrl = "https://muusikoiden.net"
 
 data Announcement =
-    Announcement { title :: String, link :: String, description :: String
-                 , author :: String, authorId :: String, price :: String
-                 , thumbnails :: [String], dates :: String }
-    deriving (Show, Eq)
+    Announcement { title :: String, announcementId :: String
+                 , description :: String, author :: String, authorId :: String
+                 , price :: String, thumbnails :: [String], dates :: String
+                 }
+    deriving (Show)
+
+instance Eq Announcement where
+    (==) a b = announcementId a == announcementId b
 
 announcements :: Scraper String [Announcement]
-announcements = chroots "table" announcement
+announcements = chroots ("table" @: ["cellpadding" @= "2"]) announcementScraper
 
-announcement :: Scraper String Announcement
-announcement = do
-    (t, l) <- chroot ("td" @: [hasClass "tori_title"] // "a") $ do
-        t <- text $ tagSelector "a"
-        l <- attr "href" $ tagSelector "a"
-        return (t, l)
+announcementScraper :: Scraper String Announcement
+announcementScraper = do
+    (t, l) <-
+        chroot ("td" @: [hasClass "tori_title"] // "a")
+        $   (,)
+        <$> (text $ tagSelector "a")
+        <*> (attr "href" $ tagSelector "a")
     desc           <- text $ "font" @: [hasClass "msg"]
-    (auth, authId) <- chroot ("a" @: ["href" @=~ (re "/jasenet.*")]) $ do
-        auth   <- text "a"
-        authId <- attr "href" $ "a"
-        return (auth, authId)
-    p    <- texts "p" -- FIXME: Price doesn't get scraped properly
+    (auth, authId) <-
+        chroot ("a" @: ["href" @=~ (re "/jasenet.*")])
+        $   (,)
+        <$> (text "a")
+        <*> (attr "href" $ "a")
+    p    <- innerHTMLs $ "p" -- FIXME: Price doesn't get scraped properly
     pics <- attrs "src" $ "img" @: [hasClass "border"]
     dat  <- text $ "small" @: [hasClass "light"]
-    return $ Announcement { title       = t
-                          , link        = baseUrl <> l
-                          , description = desc
-                          , author      = auth
-                          , authorId    = authId
-                          , price       = concat p
-                          , thumbnails  = pics
-                          , dates       = dat
+    return $ Announcement { title          = t
+                          , announcementId = baseUrl <> l
+                          , description    = desc
+                          , author         = auth
+                          , authorId       = authId
+                          , price          = concat p
+                          , thumbnails     = pics
+                          , dates          = dat
                           }
+
+scrapeAnnouncements :: URL -> IO (Maybe [Announcement])
+scrapeAnnouncements url = scrapeURL url announcements
 
 main :: IO ()
 main = do
-    scrapeURL
-            (baseUrl <> "/tori/?type=sell&province=Keski-Suomi&category=20")
-            announcements
-        >>= print
+    res <- scrapeAnnouncements
+        (baseUrl <> "/tori/?type=sell&province=Keski-Suomi&category=20")
+    case res of
+        Just anns -> mapM_ print anns
+        Nothing   -> print "Error: something went wrong"
