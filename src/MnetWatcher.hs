d3f123788248e1ccf9014a41e06d0f1aaa6d95b2
Aleksi Tarvainen <a.aleksi.tarvainen@gmail.com>
2020-06-30 15:43:02 +0300

    Handle case when no new announcements

--- src/MnetWatcher.hs
+++ src/MnetWatcher.hs
@@ -17,12 +17,12 @@ import qualified Text.Regex.TDFA               as RegexTDFA
 announcementFile :: FilePath
 announcementFile = "seenAnnouncements.txt"
 
-saveSeenAnnouncementIds :: [Announcement] -> IO ()
-saveSeenAnnouncementIds =
+saveAnnouncementIds :: [Announcement] -> IO ()
+saveAnnouncementIds =
     T.appendFile announcementFile . T.unlines . map announcementId
 
-loadSeenAnnouncementIds :: IO (Set.Set T.Text)
-loadSeenAnnouncementIds = catch
+loadAnnouncementIds :: IO (Set.Set T.Text)
+loadAnnouncementIds = catch
     (Set.fromList . T.lines <$> T.readFile announcementFile)
     (\(_ :: IOException) -> return Set.empty)
 
@@ -34,12 +34,14 @@ runApp :: IO ()
 runApp = do
     res <- scrapeAnnouncements
         (baseUrl <> "/tori/?type=sell&province=Keski-Suomi&category=20")
-    time <- formatTime defaultTimeLocale "%-d.%-m.%Y %-R" <$> getCurrentTime
-    let title = "Uusia ilmoituksia " <> time
     case res of
+        Just []   -> putStrLn "No new announcements"
         Just anns -> do
-            seen <- loadSeenAnnouncementIds
+            seen <- loadAnnouncementIds
             let newAnnouncements = filterSeenAnnouncements seen anns
+            time <-
+                formatTime defaultTimeLocale "%-d.%-m.%Y %-R" <$> getCurrentTime
+            let title = "Uusia ilmoituksia " <> time
             writeFile "out.html" $ announcementsToHtml title newAnnouncements
-            saveSeenAnnouncementIds newAnnouncements
+            saveAnnouncementIds newAnnouncements
         Nothing -> error "Error: failed to scrape"
